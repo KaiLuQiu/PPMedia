60e497ea866b855cbc94972c7e55e1961f839435
邱开禄 <944624367@qq.com>
2020-02-04 11:24:58 +0800

    [feature] add MediaBaseController class

---
+++ PPMedia/PPMedia/mediaCore/mediaKernel/MediaCodec/MediaBaseController.h
@@ -0,0 +1,66 @@
+//
+//  MediaBaseController.h
+//  PPMedia
+//
+//  Created by 邱开禄 on 2020/02/04.
+//  Copyright © 2020 邱开禄. All rights reserved.
+//
+
+#ifndef MediaBaseController_H
+#define MediaBaseController_H
+#include <string>
+#include "MediaCommon.h"
+#include "MediaContext.h"
+#include "MediaFrame.h"
+#include "MediaStream.h"
+
+NS_MEDIA_BEGIN
+
+class MediaBaseController
+{
+public:
+    MediaBaseController();
+    
+    virtual ~MediaBaseController();
+    
+     /*
+      * 设置媒体播放路径
+      */
+    virtual void setUrl(std::string mediaPath) {
+        this->mediaPath = mediaPath;
+    }
+    /*
+     * 进入准备状态
+     */
+    virtual int prepareAsync() {return 0;}
+
+    /*
+     * 开始
+     */
+    virtual int start() {return 0;}
+
+    /*
+     * 停止播放
+     */
+    virtual int stop() {return 0;}
+
+    /*
+     * seek
+     */
+    virtual int seek(float pos) {return 0;}
+
+    /*
+     * 暂停
+     */
+    virtual int pause() {return 0;}
+protected:
+    // 媒体播放上下文
+    MediaContext                *mediaContext;
+    // 媒体播放URL
+    std::string                 mediaPath;
+private:
+
+};
+
+NS_MEDIA_END
+#endif // MediaController_H
