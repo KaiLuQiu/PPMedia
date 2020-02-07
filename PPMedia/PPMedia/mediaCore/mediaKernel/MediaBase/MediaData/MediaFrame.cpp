9f633b92d6fb73ea4a3daf25614b131b932167bc
邱开禄 <944624367@qq.com>
2020-02-07 12:00:30 +0800

    [feature] Initially add MediaFrame class (encapsulate AVFrame)
    1.Initially add MediaFrame class (encapsulate AVFrame)

---
+++ PPMedia/PPMedia/mediaCore/mediaKernel/MediaBase/MediaData/MediaFrame.cpp
@@ -0,0 +1,13 @@
+//
+//  MediaFrame.cpp
+//  PPMedia
+//
+//  Created by 邱开禄 on 2020/02/03.
+//  Copyright © 2020 邱开禄. All rights reserved.
+//
+
+#include "MediaFrame.h"
+NS_MEDIA_BEGIN
+
+NS_MEDIA_END
+
---
+++ PPMedia/PPMedia/mediaCore/mediaKernel/MediaBase/MediaData/MediaFrame.h
@@ -0,0 +1,23 @@
+//
+//  MediaFrame.h
+//  PPMedia
+//
+//  Created by 邱开禄 on 2020/02/03.
+//  Copyright © 2020 邱开禄. All rights reserved.
+//
+
+#ifndef MediaFrame_H
+#define MediaFrame_H
+#include <string>
+#include "MediaCommon.h"
+
+NS_MEDIA_BEGIN
+
+// 对解码后的音视频数据进行封装
+class MediaFrame
+{
+    
+};
+
+NS_MEDIA_END
+#endif // MediaFrame_H
