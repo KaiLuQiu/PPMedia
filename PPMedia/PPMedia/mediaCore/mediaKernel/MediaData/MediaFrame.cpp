909552271043c1634055070c2da1d2b7d31a7741
邱开禄 <944624367@qq.com>
2020-02-25 21:14:24 +0800

    [feature] Encapsulation of AvFrame

--- PPMedia/PPMedia/mediaCore/mediaKernel/MediaData/MediaFrame.cpp
+++ PPMedia/PPMedia/mediaCore/mediaKernel/MediaData/MediaFrame.cpp
@@ -9,5 +9,52 @@
 #include "MediaFrame.h"
 NS_MEDIA_BEGIN
 
+MediaFrame::MediaFrame():
+frameType(PP_FRAME_NONE)
+{
+    videoConver = new (std::nothrow)VideoConver();
+    videoConver->init();
+    audioConver = NULL;
+}
+MediaFrame::~MediaFrame()
+{
+    
+}
+
+void MediaFrame::setSrcAudioParam(AudioParamInfo srcParam)
+{
+    // 如果当前的输入参数和dstAudioParam(表示音频输出的)不同的话，则初始化重采样器
+    if (dstAudioParam.channels != srcParam.channels ||
+        dstAudioParam.sample_rate != srcParam.sample_rate ||
+        dstAudioParam.frame_size != srcParam.frame_size ||
+        dstAudioParam.fmt != srcParam.fmt) {
+        // 表明输入和输出的音频格式不一样，则进行重采样操作
+        if (audioConver) {
+            audioConver->release();
+            delete audioConver;
+        }
+        audioConver = new AudioResSample();
+        // 对重采样器设置输入输出的音频参数
+        audioConver->init(srcParam, dstAudioParam);
+        
+        
+        
+    }
+//    srcAudioParam = srcParam;
+
+//    dstAudioParam = dstParam;
+}
+
+void MediaFrame::setDstAudioParam(AudioParamInfo srcParam)
+{
+    
+}
+
+void MediaFrame::setVideoParam(videoParamInfo srcParam, videoParamInfo dstParam)
+{
+    srcVideoParam = srcParam;
+    dstVideoParam = dstParam;
+}
+
 NS_MEDIA_END
 
--- PPMedia/PPMedia/mediaCore/mediaKernel/MediaData/MediaFrame.h
+++ PPMedia/PPMedia/mediaCore/mediaKernel/MediaData/MediaFrame.h
@@ -2,21 +2,64 @@
 //  MediaFrame.h
 //  PPMedia
 //
-//  Created by 邱开禄 on 2020/02/03.
+//  Created by 邱开禄 on 2020/02/11.
 //  Copyright © 2020 邱开禄. All rights reserved.
 //
 
 #ifndef MediaFrame_H
 #define MediaFrame_H
 #include <string>
-#include "MediaCommon.h"
+#include "MediaDefine.h"
+#include "MediaParamInfo.h"
+#include "AudioResSample.h"
+#include "VideoConver.h"
 
 NS_MEDIA_BEGIN
+
+typedef enum {
+    PP_FRAME_NONE,
+    PP_FRAME_VIDEO,
+    PP_FRAME_AUDIO,
+}FrameType;
 
 // 对解码后的音视频数据进行封装
 class MediaFrame
 {
+public:
+    MediaFrame();
+    ~MediaFrame();
+/************audio*************/
+    /*
+     * 设置输入输出的音频参数信息
+     */
+    void setSrcAudioParam(AudioParamInfo srcParam);
     
+    void setDstAudioParam(AudioParamInfo srcParam);
+
+/************video*************/
+    /*
+     * 设置输入输出的视频参数信息
+     */
+    void setVideoParam(videoParamInfo srcParam, videoParamInfo dstParam);
+
+
+private:
+    // 当前媒体的类型
+    FrameType                   frameType;
+    // 视频格式转换者
+    VideoConver*                videoConver;
+    // 音频重采样转换者
+    AudioResSample*             audioConver;
+    // 数据
+    uint8_t*                    data;
+    // 数据大小
+    int64_t                     size;
+    // 音视频输入输出参数
+    AudioParamInfo              srcAudioParam;
+    AudioParamInfo              dstAudioParam;
+    videoParamInfo              srcVideoParam;
+    videoParamInfo              dstVideoParam;
+    //
 };
 
 NS_MEDIA_END
