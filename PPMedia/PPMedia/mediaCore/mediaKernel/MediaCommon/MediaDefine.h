e066e227f1686a68eb654be53342f92ae8082463
邱开禄 <944624367@qq.com>
2020-03-01 22:19:58 +0800

    【feature】Add macro definition for log print level
    1.Add macro definition for log print level

--- PPMedia/PPMedia/mediaCore/mediaKernel/MediaCommon/MediaDefine.h
+++ PPMedia/PPMedia/mediaCore/mediaKernel/MediaCommon/MediaDefine.h
@@ -14,16 +14,17 @@
 
 //#define FRAME_QUEUE_SIZE 24
 // 这边的QUEUE_SIZE最好为3，否则将会导致decode thread中的丢帧效果失效，原因是queue size过大，里面的数据量多，导致VD和VO两者直接使用的最新一笔数据pts差值变大，从而导致frame_drops_early失效
+/********************************************播放器内部参数宏定义********************************************/
 #define VIDEO_PICTURE_QUEUE_SIZE 3
 #define SUBPICTURE_QUEUE_SIZE 16
 #define SAMPLE_QUEUE_SIZE 18
 #define PCM_QUEUE_SIZE FFMAX(SAMPLE_QUEUE_SIZE, FFMAX(VIDEO_PICTURE_QUEUE_SIZE, SUBPICTURE_QUEUE_SIZE))
 #define FRAME_QUEUE_SIZE FFMAX(SAMPLE_QUEUE_SIZE, FFMAX(VIDEO_PICTURE_QUEUE_SIZE, SUBPICTURE_QUEUE_SIZE))
 #define MAX_DCODEC_STREAM_NUM 5
-
-// 15M
+// 设置packetQueue的大小15M
 #define MAX_PACKET_QUEUE_SIZE (15 * 512 *1024)
 
+/********************************************对象内存安全释放********************************************/
 #ifndef SAFE_AV_FREE
 #define SAFE_AV_FREE(p) if(p != NULL) {av_free(&p); p = NULL;}
 #endif
@@ -38,6 +39,20 @@
 #define SAFE_FREE(p) if(p != NULL) {free(p); p = NULL;}
 #endif
 
+/********************************************定义打印方式********************************************/
+#define VLOG(level, TAG, ...)    ((void)vprintf(__VA_ARGS__))
+#define ALOG(level, TAG, ...)    ((void)printf(__VA_ARGS__))
+
+/********************************************定义log打印级别********************************************/
+#define PP_LOG_UNKNOWN     0
+#define PP_LOG_DEFAULT     1
+#define PP_LOG_VERBOSE     2
+#define PP_LOG_DEBUG       3
+#define PP_LOG_INFO        4
+#define PP_LOG_WARN        5
+#define PP_LOG_ERROR       6
+#define PP_LOG_FATAL       7
+#define PP_LOG_SILENT      8
 
 #endif
 
